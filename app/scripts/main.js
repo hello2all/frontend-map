37c2de7d6cc8f4bc0ed51a46a10fd13aa2d28225
hello2all <hellot2@hotmail.com>
2016-05-27 15:40:00 +0800

    build
    update eslint to 2.0.0
    build
    update git ignore

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,112 +3,112 @@ Museums data
 */
 var museums = [
   {
-    "name": "ArtScience Museum",
-    "address": "10 Bayfront Ave.",
-    "wikiurl":"https://en.wikipedia.org/wiki/ArtScience_Museum",
-    "crossStreet": "Marina Bay Sands",
-    "lat": 1.2860434088214237,
-    "lng": 103.859703540802,
-    "distance": 8444,
-    "postalCode": "018956",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["10 Bayfront Ave. (Marina Bay Sands)", "018956", "Singapore"]
+    'name': 'ArtScience Museum',
+    'address': '10 Bayfront Ave.',
+    'wikiurl': 'https://en.wikipedia.org/wiki/ArtScience_Museum',
+    'crossStreet': 'Marina Bay Sands',
+    'lat': 1.2860434088214237,
+    'lng': 103.859703540802,
+    'distance': 8444,
+    'postalCode': '018956',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['10 Bayfront Ave. (Marina Bay Sands)', '018956', 'Singapore']
   },
   {
-    "name": "SAM at 8Q",
-    "address": "8 Queen St",
-    "wikiurl":"https://en.wikipedia.org/wiki/8Q_SAM",
-    "crossStreet": "Bras Basah Rd",
-    "lat": 1.2974596023559193,
-    "lng": 103.851789,
-    "distance": 7538,
-    "postalCode": "188535",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["8 Queen St (Bras Basah Rd)", "188535", "Singapore"]
+    'name': 'SAM at 8Q',
+    'address': '8 Queen St',
+    'wikiurl': 'https://en.wikipedia.org/wiki/8Q_SAM',
+    'crossStreet': 'Bras Basah Rd',
+    'lat': 1.2974596023559193,
+    'lng': 103.851789,
+    'distance': 7538,
+    'postalCode': '188535',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['8 Queen St (Bras Basah Rd)', '188535', 'Singapore']
   },
   {
-    "name": "Asian Civilisations Museum",
-    "address": "1 Empress Pl.",
-    "wikiurl":"https://en.wikipedia.org/wiki/Asian_Civilisations_Museum",
-    "crossStreet": "Fullerton Road",
-    "lat": 1.287235894855731,
-    "lng": 103.85206308368407,
-    "distance": 7585,
-    "postalCode": "179555",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["1 Empress Pl. (Fullerton Road)", "179555", "Singapore"]
+    'name': 'Asian Civilisations Museum',
+    'address': '1 Empress Pl.',
+    'wikiurl': 'https://en.wikipedia.org/wiki/Asian_Civilisations_Museum',
+    'crossStreet': 'Fullerton Road',
+    'lat': 1.287235894855731,
+    'lng': 103.85206308368407,
+    'distance': 7585,
+    'postalCode': '179555',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['1 Empress Pl. (Fullerton Road)', '179555', 'Singapore']
   },
   {
-    "name": "National Galalery Singapore",
-    "address": "1 St. Andrew's Road",
-    "wikiurl":"https://en.wikipedia.org/wiki/National_Gallery_Singapore",
-    "lat": 1.2902051452344496,
-    "lng": 103.85153889656067,
-    "distance": 7504,
-    "postalCode": "178957",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["1 St. Andrew's Road", "178957", "Singapore"]
+    'name': 'National Galalery Singapore',
+    'address': '1 St. Andrew\'s Road',
+    'wikiurl': 'https://en.wikipedia.org/wiki/National_Gallery_Singapore',
+    'lat': 1.2902051452344496,
+    'lng': 103.85153889656067,
+    'distance': 7504,
+    'postalCode': '178957',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['1 St. Andrew\'s Road', '178957', 'Singapore']
   },
   {
-    "name": "National Museum of Singapore",
-    "address": "93 Stamford Rd",
-    "wikiurl":"https://en.wikipedia.org/wiki/National_Museum_of_Singapore",
-    "lat": 1.2967909719264816,
-    "lng": 103.84858846664429,
-    "distance": 7178,
-    "postalCode": "178897",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["93 Stamford Rd", "178897", "Singapore"]
+    'name': 'National Museum of Singapore',
+    'address': '93 Stamford Rd',
+    'wikiurl': 'https://en.wikipedia.org/wiki/National_Museum_of_Singapore',
+    'lat': 1.2967909719264816,
+    'lng': 103.84858846664429,
+    'distance': 7178,
+    'postalCode': '178897',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['93 Stamford Rd', '178897', 'Singapore']
   },
   {
-    "name": "Peranakan Museum",
-    "address": "39 Armenian St.",
-    "wikiurl":"https://en.wikipedia.org/wiki/Peranakan_Museum",
-    "lat": 1.2942303498133292,
-    "lng": 103.84923223811441,
-    "distance": 7240,
-    "postalCode": "179941",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["39 Armenian St.", "179941", "Singapore"]
+    'name': 'Peranakan Museum',
+    'address': '39 Armenian St.',
+    'wikiurl': 'https://en.wikipedia.org/wiki/Peranakan_Museum',
+    'lat': 1.2942303498133292,
+    'lng': 103.84923223811441,
+    'distance': 7240,
+    'postalCode': '179941',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['39 Armenian St.', '179941', 'Singapore']
   },
   {
-    "name": "Singapore Art Museum",
-    "address": "71 Bras Basah Rd.",
-    "wikiurl":"https://en.wikipedia.org/wiki/Singapore_Art_Museum",
-    "crossStreet": "Opp SMU",
-    "lat": 1.2972674699862363,
-    "lng": 103.85109492786576,
-    "distance": 7460,
-    "postalCode": "189555",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["71 Bras Basah Rd. (Opp SMU)", "189555", "Singapore"]
+    'name': 'Singapore Art Museum',
+    'address': '71 Bras Basah Rd.',
+    'wikiurl': 'https://en.wikipedia.org/wiki/Singapore_Art_Museum',
+    'crossStreet': 'Opp SMU',
+    'lat': 1.2972674699862363,
+    'lng': 103.85109492786576,
+    'distance': 7460,
+    'postalCode': '189555',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['71 Bras Basah Rd. (Opp SMU)', '189555', 'Singapore']
   },
   {
-    "name": "Baba House",
-    "address": "157 Neil Rd.",
-    "wikiurl":"https://en.wikipedia.org/wiki/Baba_House",
-    "lat": 1.2775352294567568,
-    "lng": 103.83728105838811,
-    "distance": 6166,
-    "postalCode": "088883",
-    "cc": "SG",
-    "city": "Singapore",
-    "country": "Singapore",
-    "formattedAddress": ["157 Neil Rd.", "088883", "Singapore"]
+    'name': 'Baba House',
+    'address': '157 Neil Rd.',
+    'wikiurl': 'https://en.wikipedia.org/wiki/Baba_House',
+    'lat': 1.2775352294567568,
+    'lng': 103.83728105838811,
+    'distance': 6166,
+    'postalCode': '088883',
+    'cc': 'SG',
+    'city': 'Singapore',
+    'country': 'Singapore',
+    'formattedAddress': ['157 Neil Rd.', '088883', 'Singapore']
   }
 ];
 
@@ -223,16 +223,16 @@ Asynchronously retrieve info from wikipedia
 function ShowWiki(marker,infowindow,wikiurl)
 {
   // construct parse url to retrieve the first section
-  var parseUrl = "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&callback=?&page=" + wikiurl.replace("https://en.wikipedia.org/wiki/", "");
-  var HTMLlink = "<p><a href='%data%'>More info...</a></p>";
+  var parseUrl = 'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&callback=?&page=' + wikiurl.replace('https://en.wikipedia.org/wiki/', '');
+  var HTMLlink = '<p><a href=\'%data%\'>More info...</a></p>';
   $.ajax({
-    type: "GET",
+    type: 'GET',
     url: parseUrl,
-    contentType: "application/json; charset=utf-8",
-    dataType: "json",
+    contentType: 'application/json; charset=utf-8',
+    dataType: 'json',
     success: function (data, textStatus, jqXHR) {
       // parse results
-      var markup = data.parse.text["*"];
+      var markup = data.parse.text['*'];
       // convert string to jquery object for further proccessing
       var i = $('<div></div>').html(markup);
 
@@ -245,12 +245,12 @@ function ShowWiki(marker,infowindow,wikiurl)
       // strip out passages
       i = i.find('p').html();
       // load results into infowindow
-      infowindow.setContent(i + HTMLlink.replace("%data%",wikiurl));
+      infowindow.setContent(i + HTMLlink.replace('%data%',wikiurl));
       infowindow.open(map,marker);
     },
     error: function () {
       // throw error message
-      alert("Error on retrieving intro from wikipedia");
+      alert('Error on retrieving intro from wikipedia');
     }
   });
 }
