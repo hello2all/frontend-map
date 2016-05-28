129d022c00d10a63ddd95dade7c58eeb62a9fc9b
hello2all <hellot2@hotmail.com>
2016-05-28 09:58:26 +0800

    implement loading icon for infowindow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -228,6 +228,7 @@ Asynchronously retrieve info from wikipedia
 function ShowWiki(marker,infowindow,wikiurl)
 {
   infowindow.setPosition(marker.getPosition());
+  infowindow.setContent('<i class="fa fa-refresh fa-spin fa-3x fa-fw"></i><span class="sr-only">Loading...</span>');
   infowindow.open(map,marker);
   // construct parse url to retrieve the first section
   var parseUrl = 'http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&callback=?&page=' + wikiurl.replace('https://en.wikipedia.org/wiki/', '');
